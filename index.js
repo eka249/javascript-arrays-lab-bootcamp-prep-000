068827b55f845315a9c0941fa74bbed9d5ac6811
Ellyn Anderson <eka249@nyu.edu>
2019-05-13 04:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
 // Add your functions and code here
